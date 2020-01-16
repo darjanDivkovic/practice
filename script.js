3e2b17c0a6e3776e06e7d22f048b614894dd45c2
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-16 15:48:52 +0100

    Arrays

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
 // Coding challenge part 4
+let names = [
+    "Darjan",
+    "Boris",
+    "Mama",
+    "Smaja"
+];
 
-// task 1
-let car = {
-    name: 'Mercedes-Benz',
-    color: 'black',
-    price: 15900
-};
-
-car.price = 17000;
-
-document.write(car.price);
-
-// task 2
-function sendMessage(){
-    document.write("We are in boys!");
-}
-
-// task 3
-let car2 = {
-    state: 'dafuq is this'
-}
-
-function changeState(){
-    car2.state = 'did it change?';
-    document.write(car2.state);
+for(let i = 0 ; i < names.length ; i++){
+    document.write(names[i]);
 }
