3a740d99a5fc4af570470c1fbd4c1911f62fa266
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-16 12:48:36 +0100

    Coding challenge part 3 done!

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let person = {
-    name: 'Dacija',
-    surname: 'Divkovic',
-    job: 'Web developer',
-    age: '21'
+// Coding challenge part 3
+
+// task 1
+function displayMessage(){
+    document.write("We in boys!");
 };
 
-document.write(person.name);
+//displayMessage();
+
+// task 2
+function square(num){
+    document.write(num*num);
+}
+
+//square(2);
+
+// task 3
+function addition(a,b,c){
+    document.write(a+b+c);
+}
+
+addition(1,2,3);
