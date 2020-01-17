e29cd885d7d93fbcefa1faf462e921eaf04993ef
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-17 20:00:17 +0100

    Created a very simple quotes generator, using js nodes

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let p = document.getElementById('par');
-let button = document.getElementById('but');
+let button = document.getElementById('b');
+let quoteContainer = document.getElementById('quoteContainer');
 
-button.addEventListener('click', changeColor);
+let quotes = [
+    'Be yourself, everyone else is already taken.',
+    'Two things are infinite: the universe and human stupidity; and Im not sure about the universe',
+    'So many books, so little time',
+    'A room without books is like a body without a soul.',
+    'You know youre in love when you cant fall asleep because reality is finally better than your dreams.'
 
-function changeColor(){
-    p.style.color = "blue";
-};
+];
+
+button.addEventListener('click', displayQuote);
+
+function displayQuote(){
+    // Create a node
+    let random = Math.floor(Math.random()*quotes.length);
+   
+    let p = document.createElement("p");
+    let pText = document.createTextNode(quotes[random]);
+
+    p.appendChild(pText);
+
+    quoteContainer.appendChild(p);
+}
