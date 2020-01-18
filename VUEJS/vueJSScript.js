758f50901361be93c33a6173e3ee0ce5a2937c4c
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-18 17:56:20 +0100

    Created a toplomjer with vue events, v-on:click=---

--- VUEJS/vueJSScript.js
+++ VUEJS/vueJSScript.js
@@ -1,17 +1,19 @@
 let app = new Vue({
+
     el: '#app',
 
     data: {
-        message: "We are in boys!",
-        name: "Dacija",
-        price: 2000,
-        path: 'https://images.unsplash.com/photo-1579338775661-7d0b8621ec83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        link: 'www.github.com/darjanDivkovic',
+        temp: 0,
     },
 
     methods: {
-        discount: function(disc){
-            return this.price - disc; 
-        }
-    }
+        inc: function(){
+            this.temp++;
+        },
+    
+        dec: function(){
+            this.temp--;
+        },
+    },
+
 });
