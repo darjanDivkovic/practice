022f074c83eec2776a4ca53c1b7bef8b3caed260
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-18 16:47:18 +0100

    Learned how to data bind with v-bind:...

--- VUEJS/vueJSScript.js
+++ VUEJS/vueJSScript.js
@@ -5,11 +5,13 @@ let app = new Vue({
         message: "We are in boys!",
         name: "Dacija",
         price: 2000,
+        path: 'https://images.unsplash.com/photo-1579338775661-7d0b8621ec83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        
     },
 
     methods: {
-        discount: function(){
-            return this.price - 100; 
+        discount: function(disc){
+            return this.price - disc; 
         }
     }
 });
