0603610b9c5625dda0b85e051ba01b7edf0a1f7f
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-21 10:24:19 +0100

    Stupid practice done.

---
+++ picGenerator/script.js
@@ -0,0 +1,27 @@
+let app = new Vue({
+
+    el: '#Generator',
+
+    data: {
+        count: 0,
+       
+        srces : [
+            "./img/pic1.jpg",
+            "./img/pic2.jpg",
+            "./img/pic3.jpg",
+            "./img/pic4.jpg",
+        ],
+    },
+
+    methods: {
+        changePic : function(){
+            if(this.count != 3){
+                this.count++
+            }
+            else{
+                this.count = 0;
+            }
+        }
+    }
+
+});
