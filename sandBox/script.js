1a01e7f094552ec2dcf2d7cd9c5b48f38e990ce3
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-21 10:51:40 +0100

    Looping in VueJS UNLOCKED!

---
+++ sandBox/script.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+    el: '#app',
+
+    data: {
+        players: [
+            {name:'Dacija', sex:'male'},
+            {name:'Semra', sex:'female'},
+            {name:'Smajke', sex:'male'},
+            {name:'Bratan', sex:'male'},
+        ]
+    }
+});
