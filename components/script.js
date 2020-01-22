d76f6f4f6adc51113a5157dfdafeec6304ed8b7b
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-22 11:50:41 +0100

    Created a square calc component and accepted user input to it through this.

--- components/script.js
+++ components/script.js
@@ -1,16 +1,31 @@
-Vue.component('mycomponent',{
-    template: '<p class="para">We are in boys</p>',
+Vue.component('squarecalc',{
+    template: '<div><h1>square calc!</h1><input type="text" ref="squareof"><button v-on:click="calculate()">Calculate</button><p>{{result}}</p></div>',
+    
+    data: function(){
+        return{
+            result:' ',
+        };
+    },
+
+    methods:{
+        calculate: function(){
+            this.result = this.$refs.squareof.value * this.$refs.squareof.value;
+        },
+    },
 
 });
+
 
 let app = new Vue({
     el: '#app',
 
     data: {
-        hi: 'msg',
+        result: ' ',
     },
 
     methods: {
-
+        calculate: function(){
+            this.result = this.$refs.squareof.value * this.$refs.squareof.value;
+        }
     }
 });
