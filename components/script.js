3aba630496e5083962438802cae9d99337f7f84f
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-22 11:24:24 +0100

    Vue components!

--- components/script.js
+++ components/script.js
@@ -1,13 +1,16 @@
+Vue.component('mycomponent',{
+    template: '<p class="para">We are in boys</p>',
 
+});
 
 let app = new Vue({
     el: '#app',
 
     data: {
-
+        hi: 'msg',
     },
 
     methods: {
-        
+
     }
 });
