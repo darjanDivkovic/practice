b66945a87b7eaa6c3ad6ed1b6e256d589a4640f6
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-22 18:00:46 +0100

    We runnin now

--- vuenode/my-project/src/App.vue
+++ vuenode/my-project/src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  
+  <h1>{{ heading }}</h1>
 </template>
+
 
 <script>
 export default {
   
   data () {
     return {
-      
+      heading: 'We are in boiiiis',
     }
   }
 }
 </script>
 
+
 <style>
+
+  h1 {
+    text-align: center;
+    font-size: 2.5em;
+    color: #232323;
+  }
 
 </style>
