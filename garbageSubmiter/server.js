2e57dcf10da4d3127e903da55f7ccd348ff34cf4
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-30 22:53:41 +0100

    proslijedio iz forme u bazu sa post metodom na serveru!

--- garbageSubmiter/server.js
+++ garbageSubmiter/server.js
@@ -19,5 +19,21 @@ app.get('/adder', (req,res)=> {
 
 app.post('/garbage/add', (req,res)=> {
     console.log(req.body);
+
+    const client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'smece',
+        password: 'darjan1234',
+        port: 5432
+    });
+
+    client.connect()
+        .then(()=> {
+            const query = 'INSERT INTO smece(smece) VALUES($1)';
+            const value = [req.body.smece];
+            client.query(query,value);
+            console.log('New entry added to smece!');
+        })
 });
 
